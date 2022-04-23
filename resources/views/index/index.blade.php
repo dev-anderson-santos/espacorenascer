ab71633181cf24c3c4d3734fc3367462d30890bb
Anderson Santos <dev.anderson.santos@gmail.com>
2022-04-23 00:57:43 -0300

    Ajustes de layout

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -17,6 +17,7 @@
         <link href="https://cdnjs.cloudflare.com/ajax/libs/SimpleLightbox/2.1.0/simpleLightbox.min.css" rel="stylesheet" />
         <!-- Core theme CSS (includes Bootstrap)-->
         <link href="css/styles.css" rel="stylesheet" />
+        <link rel="stylesheet" href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}">
     </head>
     <body id="page-top">
         <!-- Navigation-->
@@ -32,7 +33,7 @@
                         {{-- <li class="nav-item"><a class="nav-link" href="#portfolio">Localização</a></li> --}}
                         <li class="nav-item"><a class="nav-link" href="#localizacao">Localização</a></li>
                         <li class="nav-item"><a class="nav-link" href="#contact">Contato</a></li>
-                        <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">Entrar</a></li>
+                        <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">Área do Cliente</a></li>
                     </ul>
                 </div>
             </div>
@@ -71,36 +72,36 @@
             <div class="container px-4 px-lg-5">
                 <h2 class="text-center mt-0">Nossas salas</h2>
                 <hr class="divider" />
-                <div class="row gx-4 gx-lg-5">
+                {{-- <div class="row gx-4 gx-lg-5">
                     <div class="col-lg-3 col-md-6 text-center">
                         <div class="mt-5">
-                            <div class="mb-2"><i class="bi-gem fs-1 text-primary"></i></div>
+                            <div class="mb-2"><i class="bi-gem fs-1 text-theme"></i></div>
                             <h3 class="h4 mb-2">Sturdy Themes</h3>
                             <p class="text-muted mb-0">Our themes are updated regularly to keep them bug free!</p>
                         </div>
                     </div>
                     <div class="col-lg-3 col-md-6 text-center">
                         <div class="mt-5">
-                            <div class="mb-2"><i class="bi-laptop fs-1 text-primary"></i></div>
+                            <div class="mb-2"><i class="bi-laptop fs-1 text-theme"></i></div>
                             <h3 class="h4 mb-2">Up to Date</h3>
                             <p class="text-muted mb-0">All dependencies are kept current to keep things fresh.</p>
                         </div>
                     </div>
                     <div class="col-lg-3 col-md-6 text-center">
                         <div class="mt-5">
-                            <div class="mb-2"><i class="bi-globe fs-1 text-primary"></i></div>
+                            <div class="mb-2"><i class="bi-globe fs-1 text-theme"></i></div>
                             <h3 class="h4 mb-2">Ready to Publish</h3>
                             <p class="text-muted mb-0">You can use this design as is, or you can make changes!</p>
                         </div>
                     </div>
                     <div class="col-lg-3 col-md-6 text-center">
                         <div class="mt-5">
-                            <div class="mb-2"><i class="bi-heart fs-1 text-primary"></i></div>
+                            <div class="mb-2"><i class="bi-heart fs-1 text-theme"></i></div>
                             <h3 class="h4 mb-2">Made with Love</h3>
                             <p class="text-muted mb-0">Is it really open source if it's not made with love?</p>
                         </div>
                     </div>
-                </div>
+                </div> --}}
             </div>
         </section>
         <!-- Portfolio-->
@@ -135,8 +136,8 @@
                         </a>
                     </div>
                     <div class="col-lg-4 col-sm-6">
-                        <a class="portfolio-box" href="{{ asset('images/salas/04.jpeg') }}" title="Espaço Juntos">
-                            <img class="img-fluid" src="{{ asset('images/salas/04.jpeg') }}" alt="..." />
+                        <a class="portfolio-box" href="{{ asset('images/salas/07.png') }}" title="Espaço Juntos">
+                            <img class="img-fluid" src="{{ asset('images/salas/07.png') }}" alt="..." />
                             <div class="portfolio-box-caption">
                                 <div class="project-category text-white-50">Espaço</div>
                                 <div class="project-name">Juntos</div>
@@ -144,8 +145,8 @@
                         </a>
                     </div>
                     <div class="col-lg-4 col-sm-6">
-                        <a class="portfolio-box" href="{{ asset('images/salas/05.jpeg') }}" title="Espaço Juntos">
-                            <img class="img-fluid" src="{{ asset('images/salas/05.jpeg') }}" alt="..." />
+                        <a class="portfolio-box" href="{{ asset('images/salas/08.png') }}" title="Espaço Juntos">
+                            <img class="img-fluid" src="{{ asset('images/salas/08.png') }}" alt="..." />
                             <div class="portfolio-box-caption">
                                 <div class="project-category text-white-50">Espaço</div>
                                 <div class="project-name">Juntos</div>
@@ -170,7 +171,7 @@
                     <div class="col-lg-8 text-center">
                         <h2 class="text-white mt-0">Localização</h2>
                         <hr class="divider divider-light" />
-                        <p class="text-white mb-4"><b>O espaço perfeito para você, está localizado no coração de Nova Iguaçú, bem próximo a Via Light no centro comercial da cidade.</b></p>
+                        <p class="text-white mb-4"><b>O espaço perfeito para você está localizado no coração de Nova Iguaçú, bem próximo a Via Light no centro comercial da cidade.</b></p>
                         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3447.6768083693264!2d-43.446646103036635!3d-22.75960642196577!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x99670010b1275d%3A0x309493d995cb5039!2sR.%20Prof.%20Venina%20Correa%20Torres%2C%2017A%20-%20Centro%2C%20Nova%20Igua%C3%A7u%20-%20RJ%2C%2026221-200!5e0!3m2!1spt-BR!2sbr!4v1650679236821!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                     </div>
                 </div>
@@ -188,72 +189,21 @@
             <div class="container px-4 px-lg-5">
                 <div class="row gx-4 gx-lg-5 justify-content-center">
                     <div class="col-lg-8 col-xl-6 text-center">
-                        <h2 class="mt-0">Let's Get In Touch!</h2>
+                        <h2 class="mt-0">Entre em contato!</h2>
                         <hr class="divider" />
-                        <p class="text-muted mb-5">Ready to start your next project with us? Send us a messages and we will get back to you as soon as possible!</p>
-                    </div>
-                </div>
-                <div class="row gx-4 gx-lg-5 justify-content-center mb-5">
-                    <div class="col-lg-6">
-                        <!-- * * * * * * * * * * * * * * *-->
-                        <!-- * * SB Forms Contact Form * *-->
-                        <!-- * * * * * * * * * * * * * * *-->
-                        <!-- This form is pre-integrated with SB Forms.-->
-                        <!-- To make this form functional, sign up at-->
-                        <!-- https://startbootstrap.com/solution/contact-forms-->
-                        <!-- to get an API token!-->
-                        <form id="contactForm" data-sb-form-api-token="API_TOKEN">
-                            <!-- Name input-->
-                            <div class="form-floating mb-3">
-                                <input class="form-control" id="name" type="text" placeholder="Enter your name..." data-sb-validations="required" />
-                                <label for="name">Full name</label>
-                                <div class="invalid-feedback" data-sb-feedback="name:required">A name is required.</div>
-                            </div>
-                            <!-- Email address input-->
-                            <div class="form-floating mb-3">
-                                <input class="form-control" id="email" type="email" placeholder="name@example.com" data-sb-validations="required,email" />
-                                <label for="email">Email address</label>
-                                <div class="invalid-feedback" data-sb-feedback="email:required">An email is required.</div>
-                                <div class="invalid-feedback" data-sb-feedback="email:email">Email is not valid.</div>
-                            </div>
-                            <!-- Phone number input-->
-                            <div class="form-floating mb-3">
-                                <input class="form-control" id="phone" type="tel" placeholder="(123) 456-7890" data-sb-validations="required" />
-                                <label for="phone">Phone number</label>
-                                <div class="invalid-feedback" data-sb-feedback="phone:required">A phone number is required.</div>
-                            </div>
-                            <!-- Message input-->
-                            <div class="form-floating mb-3">
-                                <textarea class="form-control" id="message" type="text" placeholder="Enter your message here..." style="height: 10rem" data-sb-validations="required"></textarea>
-                                <label for="message">Message</label>
-                                <div class="invalid-feedback" data-sb-feedback="message:required">A message is required.</div>
-                            </div>
-                            <!-- Submit success message-->
-                            <!---->
-                            <!-- This is what your users will see when the form-->
-                            <!-- has successfully submitted-->
-                            <div class="d-none" id="submitSuccessMessage">
-                                <div class="text-center mb-3">
-                                    <div class="fw-bolder">Form submission successful!</div>
-                                    To activate this form, sign up at
-                                    <br />
-                                    <a href="https://startbootstrap.com/solution/contact-forms">https://startbootstrap.com/solution/contact-forms</a>
-                                </div>
-                            </div>
-                            <!-- Submit error message-->
-                            <!---->
-                            <!-- This is what your users will see when there is-->
-                            <!-- an error submitting the form-->
-                            <div class="d-none" id="submitErrorMessage"><div class="text-center text-danger mb-3">Error sending message!</div></div>
-                            <!-- Submit Button-->
-                            <div class="d-grid"><button class="btn btn-primary btn-xl disabled" id="submitButton" type="submit">Submit</button></div>
-                        </form>
-                    </div>
-                </div>
-                <div class="row gx-4 gx-lg-5 justify-content-center">
-                    <div class="col-lg-4 text-center mb-5 mb-lg-0">
-                        <i class="bi-phone fs-2 mb-3 text-muted"></i>
-                        <div>+1 (555) 123-4567</div>
+                        <p class="text-muted mb-5">Pronto para iniciar seu próximo projeto conosco? Envie-nos uma mensagem e marque uma visita.</p>
+                        <a href="https://wa.me/5521986022928" class="btn btn-primary btn-icon-split" style="background-color: rgb(69 90 100)!important; border-color:rgb(69 90 100)!important; padding: 1.25rem 2.25rem;
+                        font-size: 0.85rem;
+                        font-weight: 700;
+                        text-transform: uppercase;
+                        border: none;
+                        border-radius: 10rem;">
+                            <span class="text">Fale com a gente!</span>
+                            &nbsp;&nbsp;&nbsp;&nbsp;
+                            <span class="icon text-white-50">
+                                <i class="fab fa-whatsapp" style="font-size: 24px;"></i>
+                            </span>
+                        </a>
                     </div>
                 </div>
             </div>
@@ -273,5 +223,12 @@
         <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
         <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
         <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+
+        <link rel="stylesheet" href="https://cdn.positus.global/production/resources/robbu/whatsapp-button/whatsapp-button.css">
+        <a id="robbu-whatsapp-button" target="_blank" href="https://api.whatsapp.com/send?phone=5521986022928&text=Olá, eu gostaria de agendar um horário.">
+        <div class="rwb-tooltip">Posso Ajudar?</div>
+        <img src="https://cdn.positus.global/production/resources/robbu/whatsapp-button/whatsapp-icon.svg">
+        </a>
+        <!-- /GetButton.io widget -->
     </body>
 </html>
