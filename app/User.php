8f5e56d736076db662dd8b1969efba92ab658c75
Anderson Santos <dev.anderson.santos@gmail.com>
2022-04-24 22:34:24 -0300

    Relacionamento e campos para gravar no banco

--- app/User.php
+++ app/User.php
@@ -2,22 +2,34 @@
 
 namespace App;
 
+use App\Models\AddressModel;
+use App\Models\UserHasAddressesModel;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
     use Notifiable, SoftDeletes;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
+    protected $table = 'users';
     protected $fillable = [
-        'name', 'email', 'password',
+        'name',
+        'email',
+        'password',
+        'username',
+        'phone',
+        'email_verified_at',
+        'cpf',
+        'inscricao_crp_crm',
+        'birth_date',
+        'academic_formations',
+        'syndromes_special_situations_experience',
+        'age_range_service',
+        'approach_lines',
+        'is_admin',
     ];
 
     /**
@@ -37,4 +49,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function hasAddress(): HasOne
+    {
+        return $this->hasOne(UserHasAddressesModel::class, 'user_id', 'id');
+    }
 }
