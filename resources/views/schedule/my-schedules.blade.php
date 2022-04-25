753a9d669775bdb045de0a6fd73ee94af6c34fab
Anderson Santos <dev.anderson.santos@gmail.com>
2022-04-24 22:40:59 -0300

    Página inicial para exibir o fechamento

---
+++ resources/views/schedule/my-schedules.blade.php
@@ -0,0 +1,23 @@
+@extends('adminlte::page')
+
+@section('content')
+<section class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1>Meus horários</h1>
+            </div>
+        </div>
+    </div>
+</section>
+<div class="card">
+    <div class="card-body">
+        <div class="alert alert-info" style="font-size: 15pt" role="alert">
+            Fechamento do mês.
+        </div>
+        
+        
+    </div>
+</div>
+
+@endsection
