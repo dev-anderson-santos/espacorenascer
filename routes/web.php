6598c5d9612f82c5fbad63c45dedb8b97beb8cf6
Anderson Santos <dev.anderson.santos@gmail.com>
2022-04-24 22:42:29 -0300

    Rotas de cadastro do usuário e fechamento

--- routes/web.php
+++ routes/web.php
@@ -3,33 +3,37 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+// TODO: Remover o -- selecione -- após escolher uma data
+// TODO: Listar os usuários cadastrados
+// TODO: Criar tela dos Meus horários
+// TODO: Colocar a versão com os itens acima para ela testar
 
 Route::get('/', function () {
     return view('index.index');
-});
+})->name('index');
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home')->middleware('auth');
-Route::get('/create', 'HomeController@create')->name('guest.create');
+
 
 Route::group(['prefix' => 'app'],function () {
+
+    Route::group(['prefix' => 'user'], function () {
+        Route::get('/new', 'UserController@create')->name('user.guest-create');
+        Route::post('/store', 'UserController@store')->name('user.store');
+        Route::get('/edit/{user_id}', 'UserController@edit')->name('user.edit');
+        Route::put('/update', 'UserController@update')->name('user.update');
+
+    });
+
     Route::group(['prefix' => 'schedule', 'middleware' => 'auth'], function () {
         Route::get('/', 'ScheduleController@index')->name('schedule.index');
         Route::get('/modal-schedule', 'ScheduleController@modalSchedule')->name('schedule.modal-schedule');
         Route::post('/to-schedule', 'ScheduleController@store')->name('schedule.store');
         Route::post('/', 'ScheduleController@showSpecificShcedule')->name('schedule.show-specific-shedule');
         Route::post('/to-destroy-schedule', 'ScheduleController@destroy')->name('schedule.destroy');
+        Route::get('/my-schedules', 'ScheduleController@mySchedules')->name('schedule.my-schedules');
     });
 });
 
