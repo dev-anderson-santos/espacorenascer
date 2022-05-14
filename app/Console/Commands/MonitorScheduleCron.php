9db1e222c5104d69a7ab508f41c857d9692871a1
Anderson Santos <dev.anderson.santos@gmail.com>
2022-05-14 11:14:07 -0300

    Monitorando agendamento para finalizá-los

---
+++ app/Console/Commands/MonitorScheduleCron.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Carbon\Carbon;
+use App\Models\ScheduleModel;
+use App\Models\SettingsModel;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class MonitorScheduleCron extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'schedule:shouldfinalize';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Finaliza cada agendamento realizado verificando caso o horário de fechamento do dia já passou.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        try {
+            DB::beginTransaction();
+
+            $schedules = ScheduleModel::where('status', '!=', 'Finalizado')->get();
+            $now = Carbon::now()->format('Y-m-d');
+
+            foreach ($schedules as $schedule) {
+                if (Carbon::parse($now)->diffInDays($schedule->date, false) <= 1 && '23:00' >= SettingsModel::first()->hora_fechamento) {
+                    $schedule->update([
+                        'status' => 'Finalizado'
+                    ]);
+                }        
+            }
+
+            DB::commit();
+            
+            $this->info('Horários atualizados com sucesso!');
+
+        } catch (\Exception $e) {
+            DB::rollback();
+            $this->error($e->getMessage());
+            $this->error('Ocorreu um erro ao atualizar os horários.');
+        }
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\MonitorScheduleCron;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -13,7 +14,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        MonitorScheduleCron::class,
     ];
 
     /**
@@ -25,6 +26,9 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->command('schedule:shouldfinalize')->everyTwoMinutes();
+        // $schedule->command('schedule:shouldfinalize')->twiceDaily(8, 12);
+        // $schedule->command('schedule:shouldfinalize')->twiceDaily(16, 20);
     }
 
     /**
