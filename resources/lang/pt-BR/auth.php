1e00050bd0563ee636713e1b2895abb19842fefe
Anderson Santos <dev.anderson.santos@gmail.com>
2022-05-30 00:25:15 -0300

    Tradução login

--- resources/lang/pt-BR/auth.php
+++ resources/lang/pt-BR/auth.php
@@ -13,8 +13,24 @@ return [
     |
     */
 
-    'failed' => 'Essas credenciais não foram encontradas em nossos registros.',
-    'password' => 'A senha informada está incorreta.',
-    'throttle' => 'Muitas tentativas de login. Tente novamente em :seconds segundos.',
-
+    'failed'                      => 'Essas credenciais não foram encontradas em nossos registros.',
+    'password'                    => 'A senha informada está incorreta.',
+    'throttle'                    => 'Muitas tentativas de login. Tente novamente em :seconds segundos.',
+    'full_name'                   => 'Nome completo',
+    'email'                       => 'Email',
+    'password'                    => 'Senha',
+    'retype_password'             => 'Repita a senha',
+    'remember_me'                 => 'Lembrar-me',
+    'register'                    => 'Registrar',
+    'register_a_new_membership'   => 'Registrar-se',
+    'i_forgot_my_password'        => 'Esqueci minha senha',
+    'i_already_have_a_membership' => 'Já sou um membro',
+    'sign_in'                     => 'Entrar',
+    'log_out'                     => 'Sair',
+    'toggle_navigation'           => 'Trocar navegação',
+    'login_message'               => 'Entre para iniciar uma nova sessão',
+    'register_message'            => 'Registrar-se',
+    'password_reset_message'      => 'Recuperar senha',
+    'reset_password'              => 'Recuperar senha',
+    'send_password_reset_link'    => 'Enviar link de recuperação de senha',
 ];
--- resources/views/vendor/adminlte/auth/login.blade.php
+++ resources/views/vendor/adminlte/auth/login.blade.php
@@ -18,7 +18,7 @@
     @php( $password_reset_url = $password_reset_url ? url($password_reset_url) : '' )
 @endif
 
-@section('auth_header', __('adminlte::adminlte.login_message'))
+@section('auth_header', __('auth.login_message'))
 
 @section('auth_body')
     <form action="{{ $login_url }}" method="post">
@@ -48,7 +48,7 @@
         {{-- Password field --}}
         <div class="input-group mb-3">
             <input type="password" name="password" class="form-control {{ $errors->has('password') ? 'is-invalid' : '' }}"
-                   placeholder="{{ __('adminlte::adminlte.password') }}">
+                   placeholder="{{ __('auth.password') }}">
             <div class="input-group-append">
                 <div class="input-group-text">
                     <span class="fas fa-lock {{ config('adminlte.classes_auth_icon', '') }}"></span>
@@ -72,7 +72,7 @@
             <div class="col-12">
                 <button type=submit class="btn btn-block {{ config('adminlte.classes_auth_btn', 'btn-flat btn-primary') }}">
                     <span class="fas fa-sign-in-alt"></span>
-                    {{ __('adminlte::adminlte.sign_in') }}
+                    {{ __('auth.sign_in') }}
                 </button>
             </div>
         </div>
@@ -85,7 +85,7 @@
     @if($password_reset_url)
         <p class="my-0">
             <a href="{{ $password_reset_url }}">
-                {{ __('adminlte::adminlte.i_forgot_my_password') }}
+                {{ __('auth.i_forgot_my_password') }}
             </a>
         </p>
     @endif
@@ -94,7 +94,7 @@
     @if($register_url)
         <p class="my-0">
             <a href="{{ route('user.guest-create') }}">
-                {{ __('adminlte::adminlte.register_a_new_membership') }}
+                {{ __('auth.register_a_new_membership') }}
             </a>
         </p>
     @endif
