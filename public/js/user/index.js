3a9842bf0d08c7fe113a011d32c44b296943077a
Anderson Santos <dev.anderson.santos@gmail.com>
2022-08-02 08:35:11 -0300

    Adicionando pesquisa de clientes

--- public/js/user/index.js
+++ public/js/user/index.js
@@ -4,13 +4,26 @@ $(function() {
         // "order": [[ 0, "asc" ]],
         "bLengthChange": false, // Oculta o campo Show [10, 15,...] entries
         "paging":   false,
-        "filter": false,
+        "filter": true,
         "info":     false,
         // "scrollX": true,
         "columnDefs": [
             // { orderable: false, className: 'reorder', targets: 0 },
             { orderable: false, targets: '_all' }
         ],
+        "language": {
+            "sEmptyTable": "Nenhum registro encontrado",
+            "sInfo": "Mostrando de _START_ até _END_ de _TOTAL_ registros",
+            "sInfoEmpty": "Mostrando 0 até 0 de 0 registros",
+            "sInfoFiltered": "(Filtrados de _MAX_ registros)",
+            "sInfoPostFix": "",
+            "sInfoThousands": ".",
+            "sLengthMenu": "_MENU_ resultados por página",
+            "sLoadingRecords": "Carregando...",
+            "sProcessing": "Processando...",
+            "sZeroRecords": "Nenhum registro encontrado",
+            "sSearch": "Pesquisar",
+        },
     });
 
 
