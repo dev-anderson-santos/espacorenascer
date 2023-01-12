175f25cb9a3cfcaf0c622a89b498f340650780a7
Anderson Santos <dev.anderson.santos@gmail.com>
2023-01-11 21:38:07 -0300

    Ajuste nas cores do avulso

--- resources/views/schedule/index-administrador.blade.php
+++ resources/views/schedule/index-administrador.blade.php
@@ -48,12 +48,12 @@
                                     @php
                                         $schedule = \App\Models\ScheduleModel::where(['hour_id' => $hour->id, 'date' => $_day, 'room_id' => $room->id])->orderBy('hour_id', 'ASC')->first();
                                     @endphp                               
-                                    <td style="text-align: center">
+                                    <td style="text-align: center; {{ (!empty($schedule) && $schedule->tipo == 'Avulso') ? 'background:#17a2b8!important' : '' }}">
                                         {{-- @if (empty($schedule) && $hour->hour < \Carbon\Carbon::now()->format('H:i:s') && $_day == \Carbon\Carbon::now()->format('Y-m-d'))
                                             Indisponível
                                         @elseif (!empty($schedule)) --}}
                                         @if (!empty($schedule))
-                                            <a href="javascript:void(0)" onclick="modalGlobalOpen('{{ route('schedule.modal-schedule', ['room_id' => $room->id, 'hour_id' => $hour->id, 'user_id' => auth()->user()->id, 'data' => $_day]) }}', 'Agendamento')">
+                                            <a href="javascript:void(0)" style="{{ $schedule->tipo == 'Avulso' ? 'color:black!important' : '' }}" onclick="modalGlobalOpen('{{ route('schedule.modal-schedule', ['room_id' => $room->id, 'hour_id' => $hour->id, 'user_id' => auth()->user()->id, 'data' => $_day]) }}', 'Agendamento')">
                                                 {{ $schedule->user->name }}
                                             </a>
                                         @else
