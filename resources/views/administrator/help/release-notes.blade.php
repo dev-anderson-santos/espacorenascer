4b17dcff7f29a501925eba4fb3f58ad832865c3c
Anderson Santos <dev.anderson.santos@gmail.com>
2023-03-06 22:04:57 -0300

    Atualização das notas de release

--- resources/views/administrator/help/release-notes.blade.php
+++ resources/views/administrator/help/release-notes.blade.php
@@ -14,6 +14,25 @@
                     <div class="box box-default no-margin-bottom">
                         <div class="box-header with-border">
                             <h4 class="box-title">
+                                <a data-toggle="collapse" data-parent="#accordion" href="#r1-1-2" aria-expanded="false" class="collapsed text-bold">
+                                    Release 1.1.2 - 06/03/2023
+                                </a>
+                            </h4>
+                            <div class="box-tools pull-right">
+                                <span data-toggle="tooltip" title="" class="badge bg-blue" data-original-title="Atualizações aplicadas"></span>
+                            </div>
+                        </div>
+                        <div id="r1-1-2" class="panel-collapse collapse" aria-expanded="false" style="height: 0px;">
+                            <div class="box-body no-border">
+                                <ul>
+                                    <li>Reparo ao excluir uma sala</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="box box-default no-margin-bottom">
+                        <div class="box-header with-border">
+                            <h4 class="box-title">
                                 <a data-toggle="collapse" data-parent="#accordion" href="#r1-1-0" aria-expanded="false" class="collapsed text-bold">
                                     Release 1.1.00 - 21/02/2023
                                 </a>
