3c81a95bea773d84cd49e36f1f19e15fe314eb52
Anderson Santos <dev.anderson.santos@gmail.com>
2023-04-22 16:52:38 -0300

    Definições para e-mail

--- config/mail.php
+++ config/mail.php
@@ -74,8 +74,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'nao-responda@espacojuntos.com'),
+        'name' => env('MAIL_FROM_NAME', 'Espaço Juntos'),
     ],
 
     /*
