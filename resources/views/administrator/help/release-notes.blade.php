9ecc938ac6d31fa62f6a72528f2033351437b8b7
Anderson Santos <dev.anderson.santos@gmail.com>
2023-07-12 01:55:28 -0300

    Notas de release

--- resources/views/administrator/help/release-notes.blade.php
+++ resources/views/administrator/help/release-notes.blade.php
@@ -14,6 +14,28 @@
                     <div class="box box-default no-margin-bottom">
                         <div class="box-header with-border">
                             <h4 class="box-title">
+                                <a data-toggle="collapse" data-parent="#accordion" href="#r1-4-2" aria-expanded="false" class="collapsed text-bold">
+                                    Release 1.4.2 - 12/07/2023
+                                </a>
+                            </h4>
+                            <div class="box-tools pull-right">
+                                <span data-toggle="tooltip" title="" class="badge bg-blue" data-original-title="Atualizações aplicadas"></span>
+                            </div>
+                        </div>
+                        <div id="r1-4-2" class="panel-collapse collapse" aria-expanded="false" style="height: 0px;">
+                            <div class="box-body no-border">
+                                <ul>
+                                    <li>Ajuste ao mudar o tipo de agendamento de Fixo para Avulso. 
+                                        <p>Agora os Fixos das semanas seguintes serão removidos ao mudar para agendamento Avulso.</p>
+                                        <p>Essa correção poderá evitar a duplicata de agendamentos.</p>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="box box-default no-margin-bottom">
+                        <div class="box-header with-border">
+                            <h4 class="box-title">
                                 <a data-toggle="collapse" data-parent="#accordion" href="#r1-4-1" aria-expanded="false" class="collapsed text-bold">
                                     Release 1.4.1 - 06/07/2023
                                 </a>
