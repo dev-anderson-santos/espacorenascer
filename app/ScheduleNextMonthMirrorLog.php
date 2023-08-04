9e59305b231e631bb4c603224e81f0e13f30b0a8
Anderson Santos <dev.anderson.santos@gmail.com>
2023-08-04 16:45:55 -0300

    Melhoria no registro de log

--- app/ScheduleNextMonthMirrorLog.php
+++ app/ScheduleNextMonthMirrorLog.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class ScheduleNextMonthMirrorLog extends Model
 {
     protected $table = 'schedule_next_month_mirror_log';
-    protected $fillable = ['message', 'email'];
+    protected $fillable = ['message', 'email', 'log'];
 }
