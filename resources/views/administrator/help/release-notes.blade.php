70d21ef58376e036058f07fac530c06061404b43
Anderson Santos <dev.anderson.santos@gmail.com>
2023-11-13 10:15:56 -0300

    Notas de release

--- resources/views/administrator/help/release-notes.blade.php
+++ resources/views/administrator/help/release-notes.blade.php
@@ -14,6 +14,26 @@
                     <div class="box box-default no-margin-bottom">
                         <div class="box-header with-border">
                             <h4 class="box-title">
+                                <a data-toggle="collapse" data-parent="#accordion" href="#r1-7-2" aria-expanded="false" class="collapsed text-bold">
+                                    Release 1.7.2 - 13/11/2023
+                                </a>
+                            </h4>
+                            <div class="box-tools pull-right">
+                                <span data-toggle="tooltip" title="" class="badge bg-blue" data-original-title="Atualizações aplicadas"></span>
+                            </div>
+                        </div>
+                        <div id="r1-7-2" class="panel-collapse collapse" aria-expanded="false" style="height: 0px;">
+                            <div class="box-body no-border">
+                                <ul>
+                                    <li>Ordenar lista de datas não faturadas.</li>                               
+                                    <li>Bloquear botão salvar data não faturada após clicá-lo.</li>                               
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="box box-default no-margin-bottom">
+                        <div class="box-header with-border">
+                            <h4 class="box-title">
                                 <a data-toggle="collapse" data-parent="#accordion" href="#r1-7-1" aria-expanded="false" class="collapsed text-bold">
                                     Release 1.7.1 - 13/10/2023
                                 </a>
