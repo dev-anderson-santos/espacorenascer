d5b2c8eb373626ed8bc99a2a30e067def8ed3bd8
Anderson Barbosa dos Santos <dev.anderson.santos@gmail.com>
2023-12-01 11:29:58 -0300

    Update UserSeeder.php
    Removendo dados sensíveis

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -13,24 +13,6 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
-            'name' => 'Daniela Montechiare Gentil',
-            'username' => '@espacojuntos',
-            'email' => 'danielamontechiaregentil@gmail.com',
-            'password' => Hash::make('888123'),
-            'phone' => '21986022928',
-            'is_admin' => '1',
-            'status' => '1'
-        ]);
-
-        DB::table('users')->insert([
-            'name' => 'Anderson Barbosa dos Santos',
-            'username' => 'dev.anderson',
-            'email' => 'dev.anderson.santos@gmail.com',
-            'password' => Hash::make('!Q@W#E4r5t6y'),
-            'phone' => '21976662004',
-            'is_admin' => '1',
-            'status' => '1',
-        ]);
+        //
     }
 }
