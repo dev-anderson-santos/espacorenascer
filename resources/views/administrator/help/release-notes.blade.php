348546b821fa0043952bd36dda85055423da425d
Anderson Santos <dev.anderson.santos@gmail.com>
2025-06-03 17:44:35 -0300

    Notas de release

--- resources/views/administrator/help/release-notes.blade.php
+++ resources/views/administrator/help/release-notes.blade.php
@@ -14,6 +14,25 @@
                     <div class="box box-default no-margin-bottom">
                         <div class="box-header with-border">
                             <h4 class="box-title">
+                                <a data-toggle="collapse" data-parent="#accordion" href="#r1-8-2" aria-expanded="false" class="collapsed text-bold">
+                                    Release 1.8.2 - 03/06/2025
+                                </a>
+                            </h4>
+                            <div class="box-tools pull-right">
+                                <span data-toggle="tooltip" title="" class="badge bg-blue" data-original-title="Atualizações aplicadas"></span>
+                            </div>
+                        </div>
+                        <div id="r1-8-2" class="panel-collapse collapse" aria-expanded="false" style="height: 0px;">
+                            <div class="box-body no-border">
+                                <ul>
+                                    <li>Ajuste no relatório de cobranças.</li>                             
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="box box-default no-margin-bottom">
+                        <div class="box-header with-border">
+                            <h4 class="box-title">
                                 <a data-toggle="collapse" data-parent="#accordion" href="#r1-8-1" aria-expanded="false" class="collapsed text-bold">
                                     Release 1.8.1 - 02/04/2024
                                 </a>
