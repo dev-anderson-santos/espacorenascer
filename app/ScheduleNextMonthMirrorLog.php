13fe4efa6d4df8adbdf3ac7a06e270304e467546
Anderson Santos <anderson.santos@fiocruz.br>
2025-09-13 07:36:31 -0300

    [:sparkles:] Alterando as models para PK com UUID

--- app/ScheduleNextMonthMirrorLog.php
+++ app/ScheduleNextMonthMirrorLog.php
@@ -3,9 +3,30 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class ScheduleNextMonthMirrorLog extends Model
 {
     protected $table = 'schedule_next_month_mirror_log';
+    
+    protected $primaryKey = 'id';
+    protected $keyType = 'string';
+    public $incrementing = false;
+    
     protected $fillable = ['message', 'email', 'log'];
+
+    protected $casts = [
+        'id' => 'string',
+    ];
+
+    protected static function boot()
+    {
+        parent::boot();
+        
+        static::creating(function ($model) {
+            if (empty($model->{$model->getKeyName()})) {
+                $model->{$model->getKeyName()} = Str::uuid()->toString();
+            }
+        });
+    }
 }
